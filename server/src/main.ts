6a34fbc05684f79cbd6b6bdc9cf2ee4b5bdcf43b
Denis Kravchuk <deniskrv@gmail.com>
2023-02-21 14:30:53 +0300

    init app

---
+++ server/src/main.ts
@@ -0,0 +1,11 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+
+
+const start = async ()=>{
+    try {
+        const app= await NestFactory.create(AppModule)
+    }catch (e) {
+        console.log(e)
+    }
+}
