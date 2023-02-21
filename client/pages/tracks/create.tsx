e451a88faa348e3e9180ee9de3a991a04251b591
Denis Kravchuk <deniskrv@gmail.com>
2023-02-21 18:41:52 +0300

    init client

---
+++ client/pages/tracks/create.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from "../../layouts/MainLayout";
+
+const Create = () => {
+    return (
+        <MainLayout>
+            Загрузка треков
+        </MainLayout>
+    );
+};
+
+export default Create;
