158fc11a691284448f9b908efc5767d6697cb1de
Denis Kravchuk <deniskrv@gmail.com>
2023-02-21 19:18:53 +0300

    create track page, index page, tracks page

---
+++ client/pages/tracks/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TrackPage = () => {
+    return (
+        <div>
+            track page
+        </div>
+    );
+};
+
+    export default TrackPage;
--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import MainLayout from "../../layouts/MainLayout";
 import {Box, Button, Card, Grid} from "@mui/material";
 import {useRouter} from "next/router";
+import {ITrack} from "../../types/track";
+import TrackList from "../../components/TrackList";
 
 const Index = () => {
 const router = useRouter()
-    const tracks = [
-        {}
+    const tracks: ITrack[] = [
+        {_id: '1', name: 'Track1', artist:'Artist1', text:'Some text', listens:5, audio:'http://localhost:5000/audio/allSTARS - Things That Go Bump In The Night (dizer.net)', picture:'http://localhost:5000/image/686773.jpg', comments:[]},
+        {_id: '2', name: 'Track2', artist:'Artist1', text:'Some text', listens:5, audio:'http://localhost:5000/audio/allSTARS - Things That Go Bump In The Night (dizer.net)', picture:'http://localhost:5000/image/686773.jpg', comments:[]},
+        {_id: '3', name: 'Track3', artist:'Artist1', text:'Some text', listens:5, audio:'http://localhost:5000/audio/allSTARS - Things That Go Bump In The Night (dizer.net)', picture:'http://localhost:5000/image/686773.jpg', comments:[]},
     ]
 
     return (
@@ -19,6 +23,7 @@ const router = useRouter()
                             <Button onClick={()=>router.push('/tracks/create')}>Загрузить</Button>
                         </Grid>
                     </Box>
+                    <TrackList tracks={tracks}/>
                 </Card>
             </Grid>
         </MainLayout>
