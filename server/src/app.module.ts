c34f928e2c3a9f8ffbfb2de64c1156637db7215d
Denis Kravchuk <deniskrv@gmail.com>
2023-02-21 16:45:31 +0300

    add static folder, add audio and picture, add count and search

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import {Module} from "@nestjs/common";
-import { TrackModule } from './track/track.module';
+import {TrackModule} from './track/track.module';
 import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from 'path'
+
 
 
 @Module({
-  imports: [TrackModule,MongooseModule.forRoot('mongodb://localhost:27017/music-platform')]
+    imports: [TrackModule,
+        MongooseModule.forRoot('mongodb://localhost:27017/music-platform'),
+        FileModule,
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, 'static'),
+        }),
+    ]
 })
 export class AppModule {
 
